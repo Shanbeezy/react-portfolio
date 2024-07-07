f9af0c6a3c4e3f5c40974b99d1467fc6c9b07718
Shannon James Jr. <jay.are.sj@gmail.com>
2024-07-06 20:52:15 -0400

    about.jsx completed

---
+++ src/components/About/index.jsx
@@ -0,0 +1,27 @@
+function About() {
+    return (
+      <section className="my-5">
+        <div className="my-2">
+            <div className="profile-img my-5">
+                <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+            </div>
+            <p>
+                Welcome to my About Page!
+            </p>
+            <p>        
+                I'm excited to share my journey with you. After spending several years managing in the service industry, I decided to transition into the world of coding. The experience in customer service taught me valuable skills such as communication, problem-solving, and attention to detail. These skills are proving to be incredibly beneficial as I navigate through learning and developing software.
+            </p>
+            <p>
+                My passion for technology and continuous learning inspired me to take this leap. I believe that my background provides a unique perspective in the tech industry, allowing me to approach challenges with a fresh and diverse outlook. This website, and the projects I share here, are a testament to my dedication and enthusiasm for this new chapter in my career.
+            </p>
+            <p>
+                Thank you for visiting, and I look forward to connecting with you!
+            </p>
+        </div>
+      </section>
+    );
+  }
+  
+  export default About;
+  
+
