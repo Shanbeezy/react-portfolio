6a8f262782c257ff56e0b76eaca5bfcebba35456
Shannon James Jr. <jay.are.sj@gmail.com>
2024-07-08 23:35:51 -0400

    fixed pathing/ index.js file

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+export { default as weather } from `./projects/Weather Dashboard.png`;
+export { default as codingQuiz } from `Coding Quiz.png`;
+export { default as employeeManager } from `Employee Manager.png`;
+export { default as readmeGenerator } from `ReadMe Generator.png`;
+export { default as techBlog } from `Tech Blog.png`;
+export { default as textEditor } from `Text Editor.png`;
+export { default as regexTutorial } from `Regex Tutorial.png`;
+export { default as workScheduler } from `Work Day Scheduler.png`;
